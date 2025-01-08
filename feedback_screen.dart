8b2fe8dac18c97a8d29096f9a18dfa03d38c925d
kaveeAewra <131287227+kaveeAewra@users.noreply.github.com>
2025-01-08 08:24:22 +0530

    Create feedback_screen.dart

---
+++ feedback_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FeedbackScreen extends StatelessWidget {
+  const FeedbackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Feedback"),
+        backgroundColor: const Color.fromARGB(255, 150, 207, 237),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Your Feedback",
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 5,
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Feedback Submitted!")),
+                  );
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Submit Feedback",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
